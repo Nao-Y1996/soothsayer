7bed4346893c93258be595dfc8f0be58f6c764ef
Nao-Y1996 <60519909+Nao-Y1996@users.noreply.github.com>
2025-03-09 14:46:50 +0900

    refactor: move functions and variables from gradio_ui.py to ui.py

---
+++ app/interfaces/obs/ui.py
@@ -0,0 +1,133 @@
+import datetime
+from pydantic import BaseModel
+
+from app.domain.westernastrology import WesternAstrologyStatusEntity
+from app.domain.youtube.live import LiveChatMessageEntity
+from app.interfaces.gradio_app.constract_html import (
+    h2_tag,
+)
+
+
+class AstrologyData(BaseModel):
+    chat_message: LiveChatMessageEntity
+    status: WesternAstrologyStatusEntity
+
+
+class LatestGlobalStateView(BaseModel):
+    """
+    最新の画面表示用データを保持するオブジェクト
+    """
+
+    all_data: list[AstrologyData]
+    info_html: str
+    chat_html: str
+    astrology_result_text: str
+    current_index: int
+    play_button_name: str
+
+
+def get_jp_time(_datatime: datetime) -> str:
+    time_dt_ja = datetime.timedelta(hours=9)
+    return (_datatime + time_dt_ja).strftime("%H:%M:%S")
+
+
+def get_info_html(current_index: int, data_list: list[AstrologyData]):
+    """
+    データの情報を表示する HTML を返す。
+    """
+    current_data = data_list[current_index] if data_list else None
+    if not current_data:
+        return h2_tag("データ")
+
+    length = len(data_list)
+    return h2_tag(
+        f"データ No. {current_index + 1}/{length}（{get_jp_time(current_data.chat_message.snippet.publishedAt)}）"
+    )
+
+
+def get_chat_html(data: AstrologyData):
+    """
+    AstrologyData のチャットメッセージを HTML に整形して返す。
+    """
+
+    return get_user_name_and_comment_html(
+        data.chat_message.authorDetails.displayName,
+        data.chat_message.snippet.displayMessage,
+    )
+
+
+def get_user_name_and_comment_html(user_name: str, comment: str):
+
+    return f"""
+<div style="padding: 0; margin: 0;">
+    <span style="font-size: 1rem; font-weight: bold;">{user_name}</span>
+    <textarea readonly style="width: 100%; height: 70px; border-color: #ccc;">{comment}</textarea>
+</div>
+"""
+
+
+def get_play_button_name(data: AstrologyData | None):
+    """ """
+    if not data:
+        return "音声なし"
+    status = data.status
+    if status.result_voice_path and not status.is_played:
+        btn_name = "再生(未)"
+    elif status.result_voice_path and status.is_played:
+        btn_name = "再生(済)"
+    else:
+        btn_name = "音声なし"
+    return btn_name
+
+
+custom_css = """
+/* バックグランド処理のボタン */
+.custom-start-btn {
+    background-color: #2196F3 !important;
+    color: #FFFFFF !important;
+    round: 5px;
+}
+.custom-stop-btn {
+    background-color: #F44336 !important;
+    color: #FFFFFF !important;
+}
+.custom-play-btn {
+    background-color: #4CAF50 !important;
+    color: #FFFFFF !important;
+}
+
+/* OBS連携に関係するもの */
+.obs-info {
+    border: 1px solid #1c2b70;
+    border-radius: 6px;
+    padding: 7px;
+}
+.obs-hide-btn {
+    background-color: #e6e6e8;
+    color: #1c2b70;
+}
+.obs-update-btn {
+    background-color: #1c2b70;
+    color: #FFFFFF !important;
+}
+.obs-show-btn {
+    background-color: #e6e6e8;
+    color: #1c2b70;
+}
+
+/* 占い結果のテキスト */
+.base-info {
+    border: 1px solid #ccc;
+    border-radius: 6px;
+    padding: 7px;
+}
+.custom-astrology-html {
+    border: 1px solid #ccc;  /* 枠線 */
+    height: 300px;         /* 固定高さ（例: 300px） */
+    overflow-y: scroll;    /* スクロールバーを表示 */
+}
+span.svelte-7ddecg p {  /* 要素は実際にHTMLを確認して適切なセレクタを指定 */
+    margin-left: 10px;
+    margin-right: 10px;
+}
+"""
--- gradio_ui.py
+++ gradio_ui.py
@@ -2,12 +2,10 @@
 from app.core import logging_config  # これにより設定が適用される
 
 # isort: on
-import datetime
 from functools import wraps
 from logging import getLogger
 
 import gradio as gr
-from pydantic import BaseModel
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
 
@@ -18,8 +16,6 @@ from app.config import (
 )
 from app.core.const import GRAFANA_URL, PG_URL
 from app.domain.repositories import WesternAstrologyResultRepository
-from app.domain.westernastrology import WesternAstrologyStatusEntity
-from app.domain.youtube.live import LiveChatMessageEntity
 from app.infrastructure.db_common import initialize_db as init_db
 from app.infrastructure.repositoriesImpl import WesternAstrologyResultRepositoryImpl
 from app.interfaces.gradio_app.constract_html import (
@@ -44,27 +40,19 @@ from app.interfaces.obs.utils import (
     update_user_name,
 )
 
+from app.interfaces.obs.ui import (
+    AstrologyData,
+    LatestGlobalStateView,
+    get_info_html,
+    get_user_name_and_comment_html,
+    get_chat_html,
+    get_play_button_name,
+    custom_css,
+)
+
 logging_config.configure_logging()
 logger = getLogger(__name__)
 engine = create_engine(PG_URL, echo=False, pool_size=10, max_overflow=5)
-
-
-class AstrologyData(BaseModel):
-    chat_message: LiveChatMessageEntity
-    status: WesternAstrologyStatusEntity
-
-
-class LatestGlobalStateView(BaseModel):
-    """
-    最新の画面表示用データを保持するオブジェクト
-    """
-
-    all_data: list[AstrologyData]
-    info_html: str
-    chat_html: str
-    astrology_result_text: str
-    current_index: int
-    play_button_name: str
 
 
 def initialize_db():
@@ -88,46 +76,6 @@ def get_latest_data() -> list[AstrologyData]:
         data: AstrologyData = AstrologyData(chat_message=message, status=status)
         all_astrology_data.append(data)
     return all_astrology_data
-
-
-def get_jp_time(_datatime: datetime) -> str:
-    time_dt_ja = datetime.timedelta(hours=9)
-    return (_datatime + time_dt_ja).strftime("%H:%M:%S")
-
-
-def get_info_html(current_index: int, data_list: list[AstrologyData]):
-    """
-    データの情報を表示する HTML を返す。
-    """
-    current_data = data_list[current_index] if data_list else None
-    if not current_data:
-        return h2_tag("データ")
-
-    length = len(data_list)
-    return h2_tag(
-        f"データ No. {current_index + 1}/{length}（{get_jp_time(current_data.chat_message.snippet.publishedAt)}）"
-    )
-
-
-def get_chat_html(data: AstrologyData):
-    """
-    AstrologyData のチャットメッセージを HTML に整形して返す。
-    """
-
-    return get_user_name_and_comment_html(
-        data.chat_message.authorDetails.displayName,
-        data.chat_message.snippet.displayMessage,
-    )
-
-
-def get_user_name_and_comment_html(user_name: str, comment: str):
-
-    return f"""
-<div style="padding: 0; margin: 0;">
-    <span style="font-size: 1rem; font-weight: bold;">{user_name}</span>
-    <textarea readonly style="width: 100%; height: 70px; border-color: #ccc;">{comment}</textarea>
-</div>
-"""
 
 
 def unpack_latest_state_view(func):
@@ -260,20 +208,6 @@ def play_current_audio_ui(current_index, data_list) -> LatestGlobalStateView:
     )
 
 
-def get_play_button_name(data: AstrologyData | None):
-    """ """
-    if not data:
-        return "音声なし"
-    status = data.status
-    if status.result_voice_path and not status.is_played:
-        btn_name = "再生(未)"
-    elif status.result_voice_path and status.is_played:
-        btn_name = "再生(済)"
-    else:
-        btn_name = "音声なし"
-    return btn_name
-
-
 def update_user_info_in_obs(current_index: int, data_list: list[AstrologyData]):
     """
     OBSに表示する情報を更新して返す
@@ -292,7 +226,7 @@ def update_user_info_in_obs(current_index: int, data_list: list[AstrologyData]):
     return gr.HTML(value=get_user_name_and_comment_html(get_user_name(), get_comment()))
 
 
-def enable_user_info_in_obs(enable: bool):
+def toggle_visibility_user_info_in_obs(enable: bool):
     source_id_for_user_name = get_scene_item_id_by_name(
         OBS_SCENE_NAME, OBS_SOURCE_NAME_FOR_GROUP_OF_USER_NANE_AND_COMMENT
     )
@@ -301,65 +235,6 @@ def enable_user_info_in_obs(enable: bool):
     )
 
 
-def hide_user_info():
-    enable_user_info_in_obs(False)
-
-
-def show_user_ingo():
-    enable_user_info_in_obs(True)
-
-
-custom_css = """
-/* バックグランド処理のボタン */
-.custom-start-btn {
-    background-color: #2196F3 !important;
-    color: #FFFFFF !important;
-    round: 5px;
-}
-.custom-stop-btn {
-    background-color: #F44336 !important;
-    color: #FFFFFF !important;
-}
-.custom-play-btn {
-    background-color: #4CAF50 !important;
-    color: #FFFFFF !important;
-}
-
-/* OBS連携に関係するもの */
-.obs-info {
-    border: 1px solid #1c2b70;
-    border-radius: 6px;
-    padding: 7px;
-}
-.obs-hide-btn {
-    background-color: #e6e6e8;
-    color: #1c2b70;
-}
-.obs-update-btn {
-    background-color: #1c2b70;
-    color: #FFFFFF !important;
-}
-.obs-show-btn {
-    background-color: #e6e6e8;
-    color: #1c2b70;
-}
-
-/* 占い結果のテキスト */
-.base-info {
-    border: 1px solid #ccc;
-    border-radius: 6px;
-    padding: 7px;
-}
-.custom-astrology-html {
-    border: 1px solid #ccc;  /* 枠線 */
-    height: 300px;         /* 固定高さ（例: 300px） */
-    overflow-y: scroll;    /* スクロールバーを表示 */
-}
-span.svelte-7ddecg p {  /* 要素は実際にHTMLを確認して適切なセレクタを指定 */
-    margin-left: 10px;
-    margin-right: 10px;
-}
-"""
 with gr.Blocks(css=custom_css) as demo:
     gr.HTML(h1_tag("YouTube Live AI占い"))
 
@@ -389,10 +264,12 @@ with gr.Blocks(css=custom_css) as demo:
 
     # obsの画面の表示を切り替える
     obs_hide_btn.click(
-        fn=hide_user_info,
+        fn=toggle_visibility_user_info_in_obs,
+        inputs=[gr.State(value=False)],
     )
     obs_show_btn.click(
-        fn=show_user_ingo,
+        fn=toggle_visibility_user_info_in_obs,
+        inputs=[gr.State(value=True)],
     )
 
     obs_update_btn.click(
@@ -493,7 +370,9 @@ with gr.Blocks(css=custom_css) as demo:
         voice_status = gr.HTML(div_center_bold_text("未開始"))
         btn_voice_stop = gr.Button("音声生成 STOP", elem_classes=["custom-stop-btn"])
 
-    btn = gr.Button("データベースを初期化 (全てのバックグラウンド処理が完全に停止してから実行してください)")
+    btn = gr.Button(
+        "データベースを初期化 (全てのバックグラウンド処理が完全に停止してから実行してください)"
+    )
     btn.click(
         fn=initialize_db,
     )
